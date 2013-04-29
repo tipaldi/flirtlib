e950a9506812eb2598d0c8804acf9dbe69686a2b
tipaldi <tipaldi@4b466b05-fe88-0410-8770-c20c7260f864>
2013-04-29 17:36:37 +0000

    bug fix
    git-svn-id: https://svn.openslam.org/data/svn/flirtlib/trunk@17 4b466b05-fe88-0410-8770-c20c7260f864

--- src/feature/InterestPoint.cpp
+++ src/feature/InterestPoint.cpp
@@ -24,9 +24,10 @@
 InterestPoint::InterestPoint(const OrientedPoint2D& _position, double _scale, const Descriptor* _descriptor):
     m_position(_position),
     m_scale(_scale),
-    m_scaleLevel(0)
+    m_scaleLevel(0),
+    m_descriptor(NULL)
 {
-    m_descriptor = _descriptor->clone();
+    if(_descriptor) m_descriptor = _descriptor->clone();
 }
 
 InterestPoint::InterestPoint(const InterestPoint& _point):
